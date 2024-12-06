a2c1954e871554ca9748102e869296f50cfe22af
nicholas <nicholas.oppong@amalitech.com>
2024-12-06 23:44:00 +0000

    Created binary tree and its methods

--- src/main/java/com/example/product_management_system/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.product_management_system.repository;
-
-import com.example.product_management_system.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
-}
